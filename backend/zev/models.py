1e492747dad9ef2c8089c74b907e544f64b39cf3
Sebastian Plattner <sebastian.plattner@gmail.com>
2026-04-02 11:36:42 +0200

    chore: make meteringid unique (#103)

--- backend/zev/models.py
+++ backend/zev/models.py
@@ -169,7 +169,7 @@ class MeteringPoint(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     zev = models.ForeignKey(Zev, on_delete=models.CASCADE, related_name="metering_points")
-    meter_id = models.CharField(max_length=100, help_text="Messpunktnummer / Meter ID (e.g. CH9876543210987000000000044440859)")
+    meter_id = models.CharField(max_length=100, unique=True, help_text="Messpunktnummer / Meter ID (e.g. CH9876543210987000000000044440859)")
     meter_type = models.CharField(
         max_length=20, choices=MeteringPointType.choices, default=MeteringPointType.CONSUMPTION
     )
